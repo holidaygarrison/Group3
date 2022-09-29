51967f434e78c6a12b9d00df06fd61069e37771d
Holiday <60896633+holidaygarrison@users.noreply.github.com>
2022-09-29 18:41:17 -0500

    Update menu.php
    Updated menu design with logo

--- inc/menu.php
+++ inc/menu.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">memehub</a>
+    <a class="navbar-brand" href="">
+      <img src="inc/Logo.png" alt="MemeHub" style="max-height:40px">
+      <span class="logoFont">memehub</span>
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -11,7 +14,8 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
-        </li>
+	</li>
+<!--
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
@@ -22,9 +26,10 @@
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
-        </li>
+	</li>
+-->
       </ul>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav mb-2 mb-lg-0">
 	<li class="nav-item">
 	  <a class="nav-link" href="javascript:alert('click!');">Sign In</a>
 	</li>
