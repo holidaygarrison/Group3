ddaf5f7f2873beee416215497f6c087be4471ae8
Holiday <60896633+holidaygarrison@users.noreply.github.com>
2022-09-29 18:42:36 -0500

    Example code
    Testing/example code

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+// other includes
+
+
+
+include("inc/header.php");
+include("inc/menu.php");
+?>
+
+<div class="container container-fluid">
+  <div class="row">
+    <div class="col-sm-6 red-bg">
+	<h1 class="yellow-txt">Hello World, H1</h1>
+	<h2>Hello World, H2</h2>
+	<h3>Hello World, H3</h3>
+	<h4>Hello World. H4</h4>
+	<h5>Hello World, H5</h5>
+    </div>
+    <div class="col-sm-6 yellow-bg">
+	<h1 class="red-txt">Hello World, H1</h1>
+	<h2>Hello World, H2</h2>
+	<h3>Hello World, H3</h3>
+	<h4>Hello World. H4</h4>
+	<h5>Hello World, H5</h5>
+    </div>
+  </div>
+  <div class="row mt-4">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6 white-bg pt-3">
+      <div class="w-100 red-bg p-2"><h2>Person Name</h2></div>
+      <div class="border border-dark border-4 m-3 p-3">
+	<p> text </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<?php
+include("inc/footer.php");
+?>
