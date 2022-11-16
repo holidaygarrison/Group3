318e9113bf59c96f013e10117b8273f7b5f1ba46
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 17:51:34 -0600

    Comments

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,4 @@
+<!-- This is the header for our site that has bootstrap implemented into it adn that connects our css file to everything. -->
 <html>
      <head>
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
