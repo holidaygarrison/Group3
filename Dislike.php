2f1853cec44b184dbcfd90b7839ac3d7a0700943
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:27:50 -0600

    Comments

--- Dislike.php
+++ Dislike.php
@@ -1,13 +1,16 @@
 <?php
-
+/* Includes the needed files to create the database and posts. */
 include("inc/create_db.php");
 include("lib/posts_class.php");
 
+/* Getting the certain post and user to dislike. */
 $post = $_GET['post'];
 $user = $_GET['user'];
 
+/* Disliking that specified post. */
 Posts::DislikePost($user, $post);
 
+/* Get the posts to add to the dislike accumulator. */
 $post = Posts::GetPostInfo($post);
 echo $post['Dislikes'];
 
