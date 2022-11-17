78af7cb65ca21c690aa6eb448ca150b746a2700f
Tia <104528224+tiamalley@users.noreply.github.com>
2022-11-16 18:31:32 -0600

    Adding comments

--- SharePostPro.php
+++ SharePostPro.php
@@ -1,20 +1,24 @@
 <?php
-
+/* Including the files to create the database and posts. */
 include("inc/create_db.php");
 include("lib/posts_class.php");
 
+/* User post information. */
 $user = $_POST['user'];
 $msg = $_POST['Message'];
 $post = $_POST['post'];
 
+/* Error Handling. */
 if( !$user || !$post ){
 	echo "<Script>alert('Error!');</script>";
 	header("Location:./");
 	exit;
 }
 
+/* Shares the post */
 $post = Posts::SharePost( $user, $post, ($msg ? $msg : NULL) );
 
+/* Is located in the header. */
 header("Location:./");
 exit;
 ?>
