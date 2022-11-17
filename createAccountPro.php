87422612fa1db33ddb39f81c09f0eea9edeb94dd
Tia <104528224+tiamalley@users.noreply.github.com>
2022-11-16 18:27:17 -0600

    Adding comments

--- createAccountPro.php
+++ createAccountPro.php
@@ -1,9 +1,10 @@
 <?php
-
+/* Including the files to create the database, posts, and accounts. */
 include "inc/create_db.php";
 include "lib/posts_class.php";
 include "lib/accounts_class.php";
 
+/* User Account information. */
 $Username = $_POST['Username'];
 $PWD = $_POST['PWD'];
 $FName = $_POST['FName'];
@@ -12,10 +13,11 @@ $Gender = $_POST['Gender'];
 $BDay = $_POST['Birthday'];
 $Email = $_POST['Email'];
 
+/* Creates account for the given information. */
 $id = Accounts::CreateAccount( $Username, $PWD, $FName, $LName, $Gender, $BDay, $Email );
 
+/* Sets a cookie for five day, keeping the user loggen in. */
 if( $id ){
-	// set a cookie for five days keeping the user logged in
 	session_start();
 	$_SESSION['sid'] = session_id();
 	$_SESSION['user'] = $Username;
