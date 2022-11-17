9973922dbefcc5a557d29374288008e110d5f6fa
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:36:13 -0600

    Comments

--- FriendRequestPro.php
+++ FriendRequestPro.php
@@ -1,23 +1,27 @@
 <?php
+/* Including the files needed for the database, account, and friends. */
 include "inc/create_db.php";
 include "lib/accounts_class.php";
 include "lib/friends_class.php";
 
+/* Setting a user who sent the reuqest and the other user as the receiver of the request. */
 $requestor = $_POST['Requestor'];
 $requestee = $_POST['Requestee'];
 
+/* A user cannot send a friend request to themself. */
 if( $requestor == $requestee ){
 	echo "<script>\n".
 	     "  alert('Error: you cannot friend yourself.');\n".
 	     "  window.location = 'profile.php?d=".$requestee."';\n".
 	     "</script>\n";
 
-} else{
+} 
+/* Otherwise, the user is sending a friend request to a different user and that is okay. */
+else{
 	Friends::InitiateRequest($requestor, $requestee);
 
 	header("Location:profile.php?d=$requestee");
 	exit;
 }
-
 
 ?>
