2e9ce90af9bdf2b81f77f213b9c01774992a936d
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:23:53 -0600

    Comments

--- CreatePostPro.php
+++ CreatePostPro.php
@@ -1,19 +1,21 @@
 <?php
-
+/* Includes the files to create the database and posts. */
 include("inc/create_db.php");
 include("lib/posts_class.php");
 
+/* Getting the things needed to create the post which is the actual post, image, and text. */
 $user = $_POST['user'];
 $msg = $_POST['Message'];
 $img = $_FILES['File'];
 
+/* The user must add an image and text.*/
 if( !$img && !$msg ){
 	echo "<Script>alert('Error: need an image or message.');</script>";
 	header("Location:./");
 	exit;
 }
 
-// validate and move image
+/* Validate and move the image. */
 if($img['size'] > 0 && $img['error'] === 0){
 	$fileName = strtolower($user)."-".time()."-".$img['name'];
 	move_uploaded_file($img['tmp_name'], "./img/".$fileName);
