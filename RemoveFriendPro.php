4cc11ddac035196c0e2597cdd711a93095d31b17
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:41:42 -0600

    Comments

--- RemoveFriendPro.php
+++ RemoveFriendPro.php
@@ -1,21 +1,26 @@
 <?php
+/* Includes the needed files for the database, posts, account, and friends. */
 include( "inc/create_db.php" );
 include( "lib/posts_class.php" );
 include( "lib/accounts_class.php" );
 include( "lib/friends_class.php" );
 
+/* Sets the two users. */
 $user1 = $_POST['User1'];
 $user2 = $_POST['User2'];
 
+/* Gets the correct account information for the two users. */
 $user1 = Accounts::GetAccountInfo($user1);
 $user2 = Accounts::GetAccountInfo($user2);
 
+/* This will delete user 2 from the first users account. */
 $friends = explode(",", $user1['Friends']);
 $index = array_search( $user2['Username'], $friends );
 if( $index !== FALSE ) unset($friends[$index]);
 $user1['Friends'] = implode(",", $friends);
 Accounts::EditAccount($user1);
 
+/* This will delete user 1 from the second users account. */
 $friends = explode(",", $user2['Friends']);
 $index = array_search($user1['Username'], $friends);
 if( $index !== FALSE ) unset($friends[$index]);
