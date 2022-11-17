9dad87b9c9e9788b50ac4cec18b93efa72dfa646
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:11:47 -0600

    Comments

--- lib/posts_class.php
+++ lib/posts_class.php
@@ -1,7 +1,8 @@
 <?php
+/* This controls the posts stored in the database. */
 class Posts
 {
-
+	/* When the user adds an image and text to the create post popup, it will be stored in the database. */
 	static public function CreatePost( $uid, $msg=null, $img=null )
 	{
 		global $MHDB;
@@ -22,6 +23,7 @@ class Posts
 		return $id;
 	}
 
+	/* When the user changes anything with the Edit Post popup, it will be updated in the database. */
 	static public function EditPost( $pid, $msg=null, $img=null )
 	{
 		global $MHDB;
@@ -39,11 +41,10 @@ class Posts
 			"WHERE posts.ID = '$pid'";
 		$MHDB->query($sql);
 
-		return TRUE;
-
-		
+		return TRUE;	
 	}
 
+	/* When the user selects the delete button for a post and goes through with it, the post information will be deleted from the database. */
 	static public function DeletePost( $pid )
 	{
 		global $MHDB;
@@ -62,6 +63,7 @@ class Posts
 		return TRUE;
 	}
 
+	/* This is what pulls information about a post to display on the website. */
 	static public function GetPostInfo( $pid )
 	{
 		global $MHDB;
@@ -77,7 +79,7 @@ class Posts
 		return $result;
 	}
 
-
+	/* This selects all of the posts in the database. */
 	static public function GetAllPosts()
 	{
 		global $MHDB;
@@ -94,6 +96,7 @@ class Posts
 		return $res;
 	}
 
+	/* This will select a specific number or range of posts from the database. */
 	static public function GetPostsFor( $uid )
 	{
 		global $MHDB;
@@ -112,6 +115,7 @@ class Posts
 		return $res;
 	}
 
+	/* This will the delete the specified users post from their profile. */
 	static public function DeleteUsersPosts( $uname )
 	{
 		global $MHDB;
@@ -128,11 +132,9 @@ class Posts
 		$MHDB->query($sql);
 
 		return TRUE;
-
 	}
 
-
-
+	/* This accumulates the number of likes a post has. */
 	static public function LikePost( $uname, $pid )
 	{
 		global $MHDB;
@@ -153,12 +155,12 @@ class Posts
 			$sql = "UPDATE posts SET Likes = '$likes' WHERE posts.ID = '$pid'";
 			$MHDB->query($sql);
 			return TRUE;
-		}else{
-			return FALSE;
 		}
-		 
+		else
+			return FALSE; 
 	}
 
+	/* This accumulates the number of dislikes a post has. */
 	static public function DislikePost( $uname, $pid )
 	{
 		global $MHDB;
@@ -179,12 +181,12 @@ class Posts
 			$sql = "UPDATE posts SET Dislikes = '$dislikes' WHERE posts.ID = '$pid'";
 			$MHDB->query($sql);
 			return TRUE;
-		}else{
-			return FALSE;
 		}
-		 
+		else
+			return FALSE;
 	}
 
+	/* This accumulates the number of shares a post has. */
 	static public function SharePost( $uname, $pid, $msg=null )
 	{
 		global $MHDB;
@@ -203,6 +205,7 @@ class Posts
 		return $id;
 	}
 
+	/* This stores the comment the user made in the database. */
 	static public function MakeComment( $uname, $pid, $msg )
 	{
 		global $MHDB;
@@ -221,6 +224,7 @@ class Posts
 		return $id;
 	}
 
+	/* This gets the inputted comment information from the database to display it on the website. */
 	static public function GetCommentInfo( $cid )
 	{
 		global $MHDB;
@@ -236,6 +240,7 @@ class Posts
 		return $result;
 	}
 
+	/* This deletes a comment from the database. */
 	static public function DeleteComment( $cid )
 	{
 		global $MHDB;
@@ -251,6 +256,7 @@ class Posts
 		return TRUE;
 	}
 
+	/* This gets the comments linked to a specific post. */
 	static public function GetCommentsForPost( $pid )
 	{
 		global $MHDB;
@@ -269,11 +275,6 @@ class Posts
 
 		return $res;
 	}
-
 }
-
-
-
-
 
 ?>
