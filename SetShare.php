194cc28407b1f4bb6bfcf10e0a31a2a319e0945f
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:44:07 -0600

    Comments

--- SetShare.php
+++ SetShare.php
@@ -1,10 +1,12 @@
 <?php
-
+/* Including the needed files for the database and posts. */
 include("inc/create_db.php");
 include("lib/posts_class.php");
 
+/* Getting the post the user wants to share. */
 $post = $_GET['post'];
 
+/* Displays the post the user wanted to share. */
 $post = Posts::GetPostInfo($post);
 echo ($post['Msg'] ? "<p class='txt text-dark'>".$post['Msg']."</p>" : "").
      ($post['Img'] ? "<img src='img/".$post['Img']."' alt='img'>" : "");
