8a7b047ba37e3139a8d2589cf22b3be6e31ca000
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:38:00 -0600

    Comments

--- Like.php
+++ Like.php
@@ -1,13 +1,16 @@
 <?php
-
+/* Including the needed files for the database and posts. */
 include("inc/create_db.php");
 include("lib/posts_class.php");
 
+/* Gets the post and user who created the post. */
 $post = $_GET['post'];
 $user = $_GET['user'];
 
+/* Likes the post. */
 Posts::LikePost($user, $post);
 
+/* Accumulates the number of likes on that post. */
 $post = Posts::GetPostInfo($post);
 echo $post['Likes'];
 
