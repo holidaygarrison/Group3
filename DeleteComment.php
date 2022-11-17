e8d25c0a013d775ec74b57d8e7b9f8d4e7f57a2e
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:26:17 -0600

    Comments

--- DeleteComment.php
+++ DeleteComment.php
@@ -1,10 +1,12 @@
 <?php
-
+/* Including the needed files to create the database and posts. */
 include("inc/create_db.php");
 include("lib/posts_class.php");
 
+/* Getting the comment the user wants to delete. */
 $comm = $_GET['comment'];
 
+/* Deleting the selected comment. */
 Posts::DeleteComment($comm);
 
 ?>
