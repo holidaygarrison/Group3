42f4705882b6bbac18a6616a8f794c894607a6df
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-16 18:42:56 -0600

    Comments

--- SetEdit.php
+++ SetEdit.php
@@ -1,10 +1,12 @@
 <?php
-
+/* This includes the needed files for the database and posts. */
 include("inc/create_db.php");
 include("lib/posts_class.php");
 
+/* Gets the post the user wants to edit. */
 $post = $_GET['post'];
 
+/* Allows the user to edit the post. */
 $post = Posts::GetPostInfo($post);
 echo $post['Msg']."-;;-".$post['Img'];
 
