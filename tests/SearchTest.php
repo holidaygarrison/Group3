964f7b8e8b121341bc4a263e8832278a9fcc935e
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-30 18:23:40 -0600

    Update SearchTest.php

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -1,14 +1,29 @@
-<php
+<?php
 
-     include( "/mnt/c/xampp/htdocs/software_engineering/inc/create_db.php" );
-     include( "/mnt/c/xampp/htdocs/software_engineering/lib/posts_class.php" );
-     include( "/mnt/c/xampp/htdocs/software_engineering/lib/accounts_class.php" );
-     include( "/mnt/c/xampp/htdocs/software_engineering/lib/friends_class.php" );
+include( "/mnt/c/xampp/htdocs/software_engineering/inc/create_db.php" );
+include( "/mnt/c/xampp/htdocs/software_engineering/lib/posts_class.php" );
+include( "/mnt/c/xampp/htdocs/software_engineering/lib/accounts_class.php" );
+include( "/mnt/c/xampp/htdocs/software_engineering/lib/friends_class.php" );
+include( "/mnt/c/xampp/htdocs/software_engineering/lib/search_class.php" );
 
-     use PHPUnit\Framework\TestCase;
+
+use PHPUnit\Framework\TestCase;
+
+final class SearchTest extends TestCase
+{
+
+	public function testSearchUsers()
+	{
+		
+	}
+
+     public function testSearchPosts()
+	{
+		
+	}
      
-     final class PostsTest extends TestCase
-     {
-     
-     }
-?>
+     public function testSearchTags()
+	{
+		
+	}
+}
