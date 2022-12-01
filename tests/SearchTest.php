1501502f9b373ef9ca4f5dccf94d5c111163a1eb
AVMartin14 <104539959+AVMartin14@users.noreply.github.com>
2022-11-30 18:16:36 -0600

    Update SearchTest.php

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -1,3 +1,10 @@
 <php
 
+     include( "/mnt/c/xampp/htdocs/software_engineering/inc/create_db.php" );
+     include( "/mnt/c/xampp/htdocs/software_engineering/lib/posts_class.php" );
+     include( "/mnt/c/xampp/htdocs/software_engineering/lib/accounts_class.php" );
+     include( "/mnt/c/xampp/htdocs/software_engineering/lib/friends_class.php" );
+
+     use PHPUnit\Framework\TestCase;
+     
 ?>
